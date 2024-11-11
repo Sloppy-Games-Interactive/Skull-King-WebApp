8d1a8ec2cccc2c8d3d625a92774f3b45436c7d2e
Philippe <philippe.westenfelder@htwg-konstanz.de>
2024-11-11 19:44:15 +0100

    replace card assets

---
+++ frontend/src/components/cards/SpecialCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <img src="../../../public/images/cards/cardBackside.jpeg " alt="">
+
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/cards';
+</style>
