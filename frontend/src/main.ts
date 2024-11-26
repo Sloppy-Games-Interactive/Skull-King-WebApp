a063bc1ac7333a01ba4bc9d48a5da22c587420d2
juu <nohl.julian@gmail.com>
2024-11-26 12:23:53 +0100

    add fontawesome

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -12,13 +12,18 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {initFontAwesomeLibrary} from "@/core/icons/icons";
 
 const vuetify = createVuetify({
   components,
   directives,
 })
 
+initFontAwesomeLibrary()
+
 const app = createApp(App)
+  .component('fa-icon', FontAwesomeIcon)
 
 const api = new ApiService()
 app.provide(API_INJECTION_KEY, api);
