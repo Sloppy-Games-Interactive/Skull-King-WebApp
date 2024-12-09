47e5b67d94b84dcd476f3c5d5e1b11d7e88a0899
Philippe <philippe.westenfelder@htwg-konstanz.de>
2024-12-09 14:37:24 +0100

    add card list mouse and wheel scroll

--- frontend/src/components/cards/CardList.vue
+++ frontend/src/components/cards/CardList.vue
@@ -1,24 +1,61 @@
 <script setup lang="ts">
-import { type CardInterface, CardSize } from '@/core/model/Card'
-import Card from '@/components/cards/Card.vue'
+import { onMounted, ref } from 'vue';
+import { type CardInterface, CardSize } from '@/core/model/Card';
+import Card from '@/components/cards/Card.vue';
 
 withDefaults(
   defineProps<{
-    cards: CardInterface[]
-    onClick?: (card: CardInterface) => void
-    cardSize?: CardSize
-    hoverEffects?: boolean
+    cards: CardInterface[];
+    onClick?: (card: CardInterface) => void;
+    cardSize?: CardSize;
+    hoverEffects?: boolean;
   }>(),
   {
     cards: () => [],
     cardSize: () => CardSize.medium,
-    hoverEffects: () => false
+    hoverEffects: () => false,
   },
-)
+);
+
+const containerRef = ref<HTMLElement | null>(null);
+const isDown = ref(false);
+const startX = ref(0);
+const scrollLeft = ref(0);
+
+onMounted(() => {
+  if (containerRef.value) {
+    containerRef.value.addEventListener('wheel', (event) => {
+      event.preventDefault();
+      containerRef.value!.scrollLeft += event.deltaY;
+    });
+
+    containerRef.value.addEventListener('mousedown', (event) => {
+      isDown.value = true;
+      startX.value = event.pageX - containerRef.value!.offsetLeft;
+      scrollLeft.value = containerRef.value!.scrollLeft;
+    });
+
+    containerRef.value.addEventListener('mouseleave', () => {
+      isDown.value = false;
+    });
+
+    containerRef.value.addEventListener('mouseup', () => {
+      isDown.value = false;
+    });
+
+    containerRef.value.addEventListener('mousemove', (event) => {
+      if (!isDown.value) return;
+      event.preventDefault();
+      const x = event.pageX - containerRef.value!.offsetLeft;
+      const walk = (x - startX.value) * 2; // Adjust the scroll speed
+      containerRef.value!.scrollLeft = scrollLeft.value - walk;
+    });
+  }
+});
 </script>
 
 <template>
-  <div class="overflow-x-auto whitespace-nowrap text-center py-2">
+  <div ref="containerRef" class="overflow-x-auto whitespace-nowrap text-center py-2 cursor-grab">
     <template v-for="card in cards ?? []">
       <Card
         class="inline-block m-3 rounded-3xl"
@@ -45,5 +82,13 @@ withDefaults(
     transform: scale(1.05);
     box-shadow: 0 0 10px 2px rgba(0, 0, 0, 0.5);
   }
+}
+
+.cursor-grab {
+  cursor: grab;
+}
+
+.cursor-grab:active {
+  cursor: grabbing;
 }
 </style>
