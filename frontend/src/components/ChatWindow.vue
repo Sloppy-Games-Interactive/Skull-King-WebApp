a31dc79be8814b541f255f61a7eb2ccc1ebee0d4
Julian Nohl <nohl.julian@gmail.com>
2025-01-18 20:48:16 +0100

    chattini

---
+++ frontend/src/components/ChatWindow.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+import { useChatStore } from '@/core/stores/chatStore'
+import { computed, ref } from 'vue'
+import { ChatBus, ChatEvent, ChatEventName } from '@/core/event-bus'
+import { useLobbyStore } from '@/core/stores/lobbyStore'
+import { useEventBus } from '@vueuse/core'
+import { useGameStateStore } from '@/core/stores/gameState'
+
+const gameState = useGameStateStore()
+const lobby = useLobbyStore()
+const chat = useChatStore()
+const chatBus = useEventBus<ChatEvent>(ChatBus)
+
+const message = ref('')
+
+const sendMessage = () => {
+  if (!lobby.me || message.value.trim().length === 0) {
+    return
+  }
+  chatBus.emit(
+    new ChatEvent(ChatEventName.Message, lobby.me, message.value.trim()),
+  )
+
+  message.value = ''
+}
+
+const authorColors = computed(() => {
+  const colorMap = new Map<string, string>()
+
+  gameState.players.forEach((player, index) => {
+    colorMap.set(player.id, 'chat-author-' + (index + 1))
+  })
+
+  return colorMap
+})
+
+withDefaults(
+  defineProps<{
+    width?: number
+    height?: number
+    fill?: boolean
+  }>(),
+  {
+    width: 400,
+    height: 400,
+    fill: false,
+  },
+)
+</script>
+
+<template>
+  <div
+    class="chat bg-black/10 p-3 h-full w-full text-start leading-snug"
+    :class="fill ? '' : `max-w-[${width}px] max-h-[${height}px]`"
+  >
+    <div class="chat-messages-container overflow-y-auto">
+      <div class="chat-line" v-for="(entry, idx) of chat.messages" :key="idx">
+      <span class="chat-author" :class="authorColors.get(entry.author.id)"
+      >{{ entry.author.name }}:</span
+      >
+        <span class="chat-message text-white">{{ entry.message }}</span>
+      </div>
+    </div>
+    <v-text-field
+      class="chat-input text-white pt-1"
+      label="Send Message"
+      variant="underlined"
+      density="compact"
+      hide-details
+      @keydown.enter.prevent="sendMessage"
+      v-model="message"
+    ></v-text-field>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use 'sass:list';
+
+.chat {
+  display: grid;
+
+  font-size: 1.1rem;
+  text-shadow: rgba(51, 51, 51, 0.5) 1px 1px 2px;
+}
+
+.chat-author {
+  grid-area: author;
+}
+
+.chat-message {
+  grid-area: message;
+}
+
+.chat-line {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 0.25rem;
+  grid-template-areas: 'author message';
+  justify-content: start;
+}
+
+$chat-color-array: #7249c0, #77cd5b, #cb54ad, #cebe50, #4a304f, #ce6836,
+  #9598c4, #5a633c, #b4525a, #93c8ae;
+
+@for $i from 1 through 10 {
+  .chat-author-#{$i} {
+    color: list.nth($chat-color-array, $i);
+  }
+}
+</style>
--- frontend/src/views/pre-game/GameLobbyView.vue
+++ frontend/src/views/pre-game/GameLobbyView.vue
@@ -10,6 +10,7 @@ import AppButton from '@/components/utils/AppButton.vue'
 import PlayerStatusRow from '@/components/PlayerStatusRow.vue'
 import { useChatStore } from '@/core/stores/chatStore'
 import { ChatBus, ChatEvent, ChatEventName } from '@/core/event-bus'
+import ChatWindow from '@/components/ChatWindow.vue'
 
 const api = inject(API_INJECTION_KEY) as ApiService
 const gameState = useGameStateStore()
@@ -75,10 +76,10 @@ const copyLobbyIdToClipboard = () => {
     class="w-3/4 h-3/4 mx-auto m-10 backdrop-blur-lg"
     style="background-color: rgba(255, 255, 255, 0.5)"
   >
-    <div class="text-center gap-5">
-      <h1 class="text-6xl font-bold pt-10">Game Lobby</h1>
+    <div class="text-center gap-5 _lobby-container h-full w-full p-3">
+      <!--      <h1 class="text-6xl font-bold pt-10">Game Lobby</h1>-->
 
-      <div class="grid place-items-center my-8">
+      <div class="grid place-items-center my-8 _players">
         <PlayerStatusRow
           v-for="(player, idx) in gameState.players"
           :key="player.id"
@@ -88,45 +89,53 @@ const copyLobbyIdToClipboard = () => {
         />
       </div>
 
-      <div>
-        <!-- Chat here -->
-        <div
-          v-for="(msg, idx) in chat.messages"
-          :key="idx">
-          {{ msg.author.name }}: {{ msg.message }}
-        </div>
-
-        <div class="grid place-items-center">
-          <v-textarea
-            v-model="message"
-            @keydown.enter.prevent="sendMessage"
-            placeholder="Type a message"
-            class="w-3/4"
-          ></v-textarea>
-        </div>
+      <div class="_chat">
+        <ChatWindow fill class="rounded-l" />
       </div>
 
-      <AppButton @click="copyLobbyIdToClipboard" style="margin-bottom: 50px">
-        {{ copyBtnText }}
-      </AppButton>
+      <div class="_buttons">
+        <div class="grid grid-flow-col align-center justify-center gap-10 pb-2">
+          <AppButton @click="copyLobbyIdToClipboard">
+            {{ copyBtnText }}
+          </AppButton>
+          <AppButton
+            v-if="lobby.isHost"
+            :disabled="!hasEnoughPlayers"
+            :class="{ disabled: !hasEnoughPlayers }"
+            @click.stop.prevent="startGame"
+            >Start Game</AppButton
+          >
 
-      <div>
-        <AppButton
-          v-if="lobby.isHost"
-          :disabled="!hasEnoughPlayers"
-          :class="{ disabled: !hasEnoughPlayers }"
-          @click.stop.prevent="startGame"
-          >Start Game</AppButton
-        >
-
-        <div v-else>
-          <p class="text-2xl font-semibold px-3">
-            Waiting for host to start the game
-          </p>
+          <div v-else>
+            <p class="text-2xl font-semibold px-3">
+              Waiting for host to start the game
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </v-card>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+._lobby-container {
+  display: grid;
+  grid-template-areas:
+    'players chat'
+    'buttons buttons';
+  grid-template-rows: minmax(0, 1fr) auto;
+  grid-template-columns: auto 1fr;
+}
+
+._players {
+  grid-area: players;
+}
+
+._chat {
+  grid-area: chat;
+}
+
+._buttons {
+  grid-area: buttons;
+}
+</style>
